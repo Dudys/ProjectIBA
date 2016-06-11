5e775b660ce359f5b1c72c76e95a0d5048d767b4
Jan Duda <jan.duda27@gmail.com>
2016-06-11 10:42:45 +0200

    finished level 3

--- src/main/java/cz/duda/projectiba/servlet/Servlet.java
+++ src/main/java/cz/duda/projectiba/servlet/Servlet.java
@@ -1,20 +1,39 @@
 package cz.duda.projectiba.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Servlet extends HttpServlet{
 
+    private static final String INDEX_JSP = "/index.jsp";
+
     public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException{
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<body>");
-        out.println("<h1>Hello IBA!</h1>");
-        out.println("</body>");
-        out.println("</html>");
+            throws IOException, ServletException {
+
+        String param = request.getParameter("x");
+        if (param != null){
+            try {
+                int x = Integer.parseInt(param);
+                request.setAttribute("x", x);
+                request.getRequestDispatcher(INDEX_JSP).forward(request, response);
+            } catch (NumberFormatException ex) {
+                request.setAttribute("error", "Wrong format of param x, x should be a number!");
+                request.getRequestDispatcher(INDEX_JSP).forward(request, response);
+            } catch (ServletException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                request.setAttribute("x", 1);
+                request.getRequestDispatcher(INDEX_JSP).forward(request, response);
+            } catch (ServletException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
